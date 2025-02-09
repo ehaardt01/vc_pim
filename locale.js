c611458a1931f7d45cc671e8e5851c2fc586dc62
ehaardt01 <emmanuel.haardt@gmail.com>
2025-02-10 00:17:20 +0100

    init

--- locale.js
+++ locale.js
@@ -2,7 +2,7 @@
 function beeceptor(content) {
     const DOMAIN = 'https://virbac-pim.free.beeceptor.com/product/create_or_update?locale=fr-FR';
     const METHOD = 'POST';
-    web_request(DOMAIN + path, METHOD, JSON.stringify(content)); // Ensure JSON payload
+    web_request(DOMAIN, METHOD, JSON.stringify(content)); // Ensure JSON payload
 }
 
 const result = {

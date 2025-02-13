785ae785aa7acdabe256df50f1484e71f316f423
ehaardt01 <emmanuel.haardt@gmail.com>
2025-02-13 18:02:43 +0100

    init

--- send.js
+++ send.js
@@ -22,35 +22,14 @@ function send_to_recipient_API(path, content) {
     let response = web_request(URL, METHOD, content, HEADERS, OPTIONS);
     if (response === undefined) {
         response = {};
+    } else {
+        if ((response.code < 200) || (response.code > 299)) {
+            response["returned_status"] = "error " + response.code;
+        } else {
+            response["returned_status"] = "success " + response.code;
+        }
     }
     return response;
-
-    // function concatenateProperties(obj) {
-    //     let properties = "";
-    //     function parseObj(currentObj, parentKey = "") {
-    //         for (const key in currentObj) {
-    //             const fullKey = parentKey ? `${parentKey}.${key}` : key;
-    //             if (typeof currentObj[key] === 'object' && currentObj[key] !== null) {
-    //                 parseObj(currentObj[key], fullKey);
-    //             } else {
-    //                 properties += ` - ${fullKey}: ${currentObj[key]}\n`;
-    //             }
-    //         }
-    //     }
-    //     parseObj(obj);
-    //     return properties.trim();
-    // }
-    // const result = concatenateProperties(response);
-
-    // if ((response.status < 200) || (response.status > 299)) {
-    //     return "Resolution A";
-    //     // throw new Error("Manual error message");
-    //     // We encountered a 'Error' with message: '- message: Unable to complete web request due to exception.'. Please double check your syntax.
-    // } else {
-    //     return "Resolution B";
-    //     // throw new Error("Manual success message");
-    //     // We encountered a 'Error' with message: '- message: Success'. Please double check your syntax.
-    // }
 }
 
 function main() {

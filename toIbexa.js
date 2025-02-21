f409206f3fa6de1c23f006b5fbac243a08f0e7a9
ehaardt01 <emmanuel.haardt@gmail.com>
2025-02-21 16:18:24 +0100

    test

--- toIbexa.js
+++ toIbexa.js
@@ -35,7 +35,7 @@ const properties = [
     {name: "B2B Short description", type: "string", export_name: "b2b_short_description"},
     {name: "SEO Product Title", type: "string", export_name: "seo_product_title"},
     {name: "Marketing Product name", type: "string", export_name: "marketing_product_name"},
-    {name: "FAQ data table", type: "product", export_name: "faq_data_table", values: [{name: "FAQ reference - Question ", type: "string", export_name: "question"}, {name: "FAQ reference - Answer", type: "string", export_name: "answer"}]},
+    {name: "FAQ data table", type: "product", export_name: "faq_data_table", values: [{name: "FAQ reference - Question", type: "string", export_name: "question"}, {name: "FAQ reference - Answer", type: "string", export_name: "answer"}]},
     {name: "Country Markets", type: "enumerated", export_name: "country_markets"},
     {name: "Group Species", type: "enumerated", export_name: "group_species"},
     {name: "Animal stage", type: "enumerated", export_name: "animal_stage"},

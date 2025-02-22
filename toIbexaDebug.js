4c3389d3ee2f6dd72f4b42d898cccdd66dbd5898
ehaardt01 <emmanuel.haardt@gmail.com>
2025-02-22 07:46:14 +0100

    test

--- toIbexaDebug.js
+++ toIbexaDebug.js
@@ -2,5 +2,5 @@ DEBUG = true;
 fetch('https://raw.githubusercontent.com/ehaardt01/vc_pim/refs/heads/main/toIbexa.js')
     .then(response => response.text())
     .then(scriptText => {
-        eval(scriptText); // Attention : utiliser eval peut poser des risques de sécurité
+        eval(scriptText);
     });

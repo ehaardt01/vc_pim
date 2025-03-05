1b01ace95e47f66a7290373bf730bc4d7352b61a
ehaardt01 <emmanuel.haardt@gmail.com>
2025-03-05 10:19:13 +0100

    ajout

--- toIbexa.js
+++ toIbexa.js
@@ -69,7 +69,7 @@ const properties = SYSTEM_PROPERTIES.concat([
     {name: "Type of food", type: "enumerated", export_name: "type_of_food"},
     {name: "Neutered", type: "enumerated", export_name: "neutered"},
     {name: "Replaced Product", type: "product", export_name: "replaced_product"},
-    {name: "Key Ingredients", type: "rich_text", export_name: "key_ingredients"}
+    {name: "Key ingredients", type: "rich_text", export_name: "key_ingredients"}
 ]);
 
 /**

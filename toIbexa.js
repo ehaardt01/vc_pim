aaa1e1fb491aa433e1b59585e72e80d6361ad313
Damien <damien.venot@gmail.com>
2025-04-16 17:07:08 +0200

    Add files via upload
    add new attribute exported to ibexa

--- toIbexa.js
+++ toIbexa.js
@@ -87,7 +87,8 @@ const properties = SYSTEM_PROPERTIES.concat([
     {name: "Type of food", type: "enumerated", export_name: "type_of_food"},
     {name: "Neutered", type: "enumerated", export_name: "neutered"},
     {name: "Weight Ranges", type: "string", export_name: "weight_ranges"},
-    {name: "Key ingredients", type: "rich_text", export_name: "key_ingredients"}
+    {name: "Key ingredients", type: "rich_text", export_name: "key_ingredients"},
+    {name: "Product categories", type: "enumerated", export_name: "product_categories"}
 ]);
 
 /**
